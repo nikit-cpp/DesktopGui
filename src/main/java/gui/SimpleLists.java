a421fcea7c2811610211613e398a9e44e02e2d43
nik <nikit.cpp@yandex.ru>
2015-03-16 22:50:41 +0300

    какие-то изменения

--- src/main/java/gui/SimpleLists.java
+++ src/main/java/gui/SimpleLists.java
@@ -3,9 +3,11 @@ package gui;
 //SimpleLists.java
 //Простейший способ создания списков
 import javax.swing.*;
+
 import java.util.*;
 
 public class SimpleLists extends JFrame {
+	private static final long serialVersionUID = 1L;
 
 	public SimpleLists() {
 		super("SimpleLists");
@@ -13,11 +15,11 @@ public class SimpleLists extends JFrame {
 		// создаем списки
 		JPanel contents = new JPanel();
 		// динамически наполним вектор
-		Vector big = new Vector();
+		Vector<String> big = new Vector<String>();
 		for (int i = 0; i < 50; i++) {
 			big.add("# " + i);
 		}
-		JList bigList = new JList(big);
+		JList<Vector<String>> bigList = new JList(big);
 		//bigList.setPrototypeCellValue("12345");
 		// добавим списки в панель
 		contents.add(new JScrollPane(bigList));
