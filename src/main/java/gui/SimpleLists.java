1aea7e519a15e93be9bbb33a082f0cdffd321e9a
nik <nikit.cpp@yandex.ru>
2015-03-17 00:31:11 +0300

    картинки как null

--- src/main/java/gui/SimpleLists.java
+++ src/main/java/gui/SimpleLists.java
@@ -1,9 +1,9 @@
 package gui;
 
-//SimpleLists.java
-//Простейший способ создания списков
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.log4j.Logger;
 
 import com.github.nikit.cpp.player.PlayList;
 import com.github.nikit.cpp.player.Song;
@@ -12,74 +12,54 @@ import vk.CurlXPath;
 import vk.CurlXPathException;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.*;
 
 public class SimpleLists extends JPanel {
-
+	
+	// TODO сделать множество групп
 	private static final String GROUP_NAME = "rockmetal80";
-
+	
+	private static Logger LOGGER = Logger.getLogger(SimpleLists.class);
 	private static final long serialVersionUID = 1L;
 	private JList list;
 
-	private DefaultListModel model;
 
 	public SimpleLists() throws ParserConfigurationException,
 			CurlXPathException {
 
 		CurlXPath cxp = new CurlXPath();
 		Collection<PlayList> cpl = cxp.getPlayListsFromGroup(GROUP_NAME);
-		PlayList result = new PlayList();
-		for (PlayList pl : cpl) {
-			List<Song> songs = pl.getSongs();
-			for (Song s : songs) {
-				result.addSong(s);
-			}
-		}
 
 		setLayout(new BorderLayout());
-		model = new DefaultListModel();
-		list = new JList(model);
+		final DatabaseListModel dblm = new DatabaseListModel(cpl);
+		list = new JList(dblm);
 		JScrollPane pane = new JScrollPane(list);
-		JButton addButton = new JButton("Add Element");
-		for (int i = 0; i < result.size(); i++) {
-			model.addElement(result.getSongs().get(i));
-		}
-		
+
 		list.addMouseListener(new MouseListener() {
-			
+
 			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			public void mouseClicked(MouseEvent e) {
-		        if (e.getClickCount() == 2) {
-		            int index = list.locationToIndex(e.getPoint());
-		            System.out.println("Double clicked on Item " + index);
-		         }
+				if (e.getClickCount() == 2) {
+					int index = list.locationToIndex(e.getPoint());
+					LOGGER.debug("Double clicked on item " + index + " " + dblm.getElementAt(index));
+				}
 			}
 		});
 
-		add(pane, BorderLayout.NORTH);
+		add(pane, BorderLayout.CENTER); // CENTER раскукоживает
 	}
 
 	public static void main(String[] args) throws ParserConfigurationException,
@@ -89,5 +69,47 @@ public class SimpleLists extends JPanel {
 		frame.setContentPane(new SimpleLists());
 		frame.setSize(500, 500);
 		frame.setVisible(true);
+	}
+}
+
+
+class DatabaseListModel extends AbstractListModel {
+	// здесь будем хранить данные
+	private ArrayList data = new ArrayList();
+
+	public DatabaseListModel(Collection<PlayList> pls){
+		setDataSource(pls);
+	}
+	private void setDataSource(Collection<PlayList> pls) {
+		try {
+			// получаем данные
+			data.clear();
+			for (PlayList pl: pls) {
+				synchronized (data) {
+					List<Song> songs = pl.getSongs();
+					for (Song s : songs) {
+						data.add(s);
+					}
+				}
+				// оповещаем виды (если они есть)
+				fireIntervalAdded(this, 0, data.size());
+			}
+			fireContentsChanged(this, 0, data.size());
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	// методы модели для выдачи данных списку
+	public int getSize() {
+		synchronized (data) {
+			return data.size();
+		}
+	}
+
+	public Object getElementAt(int idx) {
+		synchronized (data) {
+			return data.get(idx);
+		}
 	}
 }
