65be2c5c058ff56ade84e10e3ab5ea3db7841b51
nik <nikit.cpp@yandex.ru>
2015-03-18 17:52:26 +0300

    JUnit interrupts Spark

--- src/test/java/spark/SparkVkEmulatorTest.java
+++ src/test/java/spark/SparkVkEmulatorTest.java
@@ -5,10 +5,11 @@ import static spark.Spark.*;
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
 import spark.*;
 
-public class SparkMain {
+public class SparkVkEmulatorTest {
 	// curl http://localhost:4567/method/groups.getById?group_ids=legends_of_rock
 	/*public static void main(String[] args) {
 		get("/method/:methodName", (req, res) -> {
@@ -21,7 +22,8 @@ public class SparkMain {
 	
 	// https://api.vk.com/method/wall.get.xml?owner_id=-11081630
 	// http://localhost:4567/method/wall.get.xml?owner_id=-11081630
-	public static void main(String[] args) {
+	@Test
+	public void testGettingWall() {
 		get("/method/:methodName", (req, res) -> {
 			String methodName = req.params(":methodName");
 			String parameterValue = req.queryParams("owner_id");
