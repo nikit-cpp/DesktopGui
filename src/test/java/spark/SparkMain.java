ed9fa5c50399b268906d1c5bf84f9ec9ec2cb40e
nik <nikit.cpp@yandex.ru>
2015-03-18 17:26:35 +0300

    too works

--- src/test/java/spark/SparkMain.java
+++ src/test/java/spark/SparkMain.java
@@ -5,10 +5,10 @@ import static spark.Spark.*;
 import spark.*;
 
 public class SparkMain {
-
+	// curl http://localhost:4567/method/groups.getById?group_ids=legends_of_rock
 	public static void main(String[] args) {
-		get("/method/:groups.getById", (req, res) -> {
-			String methodName = req.params(":groups.getById");
+		get("/method/:methodName", (req, res) -> {
+			String methodName = req.params(":methodName");
 			String parameterValue = req.queryParams("group_ids");
 			
 			return "method=" + methodName + " parameterValue=" + parameterValue;
