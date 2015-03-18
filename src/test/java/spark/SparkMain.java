129fd3a979049889ab7c3ed027c5c63f9c5df042
nik <nikit.cpp@yandex.ru>
2015-03-18 17:43:02 +0300

    неверный тип ответа как минимум

--- src/test/java/spark/SparkMain.java
+++ src/test/java/spark/SparkMain.java
@@ -2,14 +2,34 @@ package spark;
 
 import static spark.Spark.*;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
 import spark.*;
 
 public class SparkMain {
 	// curl http://localhost:4567/method/groups.getById?group_ids=legends_of_rock
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		get("/method/:methodName", (req, res) -> {
 			String methodName = req.params(":methodName");
 			String parameterValue = req.queryParams("group_ids");
+			
+			return "method=" + methodName + " parameterValue=" + parameterValue;
+		});
+	}*/
+	
+	// https://api.vk.com/method/wall.get.xml?owner_id=-11081630
+	// http://localhost:4567/method/wall.get.xml?owner_id=-11081630
+	public static void main(String[] args) {
+		get("/method/:methodName", (req, res) -> {
+			String methodName = req.params(":methodName");
+			String parameterValue = req.queryParams("owner_id");
+			
+			if (methodName.equals("wall.get.xml") && parameterValue.equals("-11081630")) {
+				return FileUtils.readFileToString(new File("src/test/resources/responce.txt"), "UTF8");
+			}
+			
 			
 			return "method=" + methodName + " parameterValue=" + parameterValue;
 		});
