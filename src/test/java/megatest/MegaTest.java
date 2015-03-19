cd2e2047c5dbc80345b57c70143e7d40b028a705
nik <nikit.cpp@yandex.ru>
2015-03-19 15:10:38 +0300

    xml outputting

--- src/test/java/megatest/MegaTest.java
+++ src/test/java/megatest/MegaTest.java
@@ -3,6 +3,7 @@ package megatest;
 // http://stackoverflow.com/questions/14179746/unit-test-a-servlet-with-an-embedded-jetty/14181904#14181904
 // http://git.eclipse.org/c/jetty/org.eclipse.jetty.project.git/tree/examples/embedded/src/main/java/org/eclipse/jetty/embedded/MinimalServlets.java
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -10,8 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.Test;
 
 public class MegaTest {
@@ -23,8 +26,8 @@ public class MegaTest {
 		ServletHandler handler = new ServletHandler();
 		server.setHandler(handler);
 
-		handler.addServletWithMapping(HelloServlet.class, "/*");
-
+		ServletHolder sh = new ServletHolder(new HelloServlet("text/xml", FileUtils.readFileToString(new File("src/test/resources/responce.txt"))));
+		handler.addServletWithMapping(sh, "/*");
 		server.start();
 
 		server.join();
@@ -32,13 +35,19 @@ public class MegaTest {
 
 	@SuppressWarnings("serial")
 	public static class HelloServlet extends HttpServlet {
+		
+		private String content;
+		private String type;
+		
+		public HelloServlet(String content, String type) {
+			this.content = content;
+			this.type = type;
+		}
 		@Override
-		protected void doGet(HttpServletRequest request,
-				HttpServletResponse response) throws ServletException,
-				IOException {
-			response.setContentType("text/html");
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			response.setContentType(content);
 			response.setStatus(HttpServletResponse.SC_OK);
-			response.getWriter().println("<h1>Hello from HelloServlet</h1>");
+			response.getWriter().write(type);
 		}
 	}
 }
