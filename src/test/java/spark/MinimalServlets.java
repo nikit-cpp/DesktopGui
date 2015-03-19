e3274014d2e496ce0469731361978d03db9a6e8e
nik <nikit.cpp@yandex.ru>
2015-03-19 14:40:23 +0300

    removed stuff comments

--- src/test/java/spark/MinimalServlets.java
+++ src/test/java/spark/MinimalServlets.java
@@ -1,23 +1,5 @@
 package spark;
 
-//
-//========================================================================
-//Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
-//------------------------------------------------------------------------
-//All rights reserved. This program and the accompanying materials
-//are made available under the terms of the Eclipse Public License v1.0
-//and Apache License v2.0 which accompanies this distribution.
-//
-//  The Eclipse Public License is available at
-//  http://www.eclipse.org/legal/epl-v10.html
-//
-//  The Apache License v2.0 is available at
-//  http://www.opensource.org/licenses/apache2.0.php
-//
-//You may elect to redistribute this code under either of these licenses.
-//========================================================================
-//
-
 // http://stackoverflow.com/questions/14179746/unit-test-a-servlet-with-an-embedded-jetty/14181904#14181904
 // http://git.eclipse.org/c/jetty/org.eclipse.jetty.project.git/tree/examples/embedded/src/main/java/org/eclipse/jetty/embedded/MinimalServlets.java
 
@@ -31,51 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
-public class MinimalServlets
-{
-public static void main( String[] args ) throws Exception
-{
-    // Create a basic jetty server object that will listen on port 8080.
-    // Note that if you set this to port 0 then a randomly available port
-    // will be assigned that you can either look in the logs for the port,
-    // or programmatically obtain it for use in test cases.
-    Server server = new Server(8080);
+public class MinimalServlets {
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
 
-    // The ServletHandler is a dead simple way to create a context handler
-    // that is backed by an instance of a Servlet.
-    // This handler then needs to be registered with the Server object.
-    ServletHandler handler = new ServletHandler();
-    server.setHandler(handler);
+		ServletHandler handler = new ServletHandler();
+		server.setHandler(handler);
 
-    // Passing in the class for the Servlet allows jetty to instantiate an
-    // instance of that Servlet and mount it on a given context path.
+		handler.addServletWithMapping(HelloServlet.class, "/*");
 
-    // IMPORTANT:
-    // This is a raw Servlet, not a Servlet that has been configured
-    // through a web.xml @WebServlet annotation, or anything similar.
-    handler.addServletWithMapping(HelloServlet.class, "/*");
+		server.start();
 
-    // Start things up!
-    server.start();
+		server.join();
+	}
 
-    // The use of server.join() the will make the current thread join and
-    // wait until the server is done executing.
-    // See
-    // http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
-    server.join();
-}
-
-@SuppressWarnings("serial")
-public static class HelloServlet extends HttpServlet
-{
-    @Override
-    protected void doGet( HttpServletRequest request,
-                          HttpServletResponse response ) throws ServletException,
-                                                        IOException
-    {
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Hello from HelloServlet</h1>");
-    }
-}
+	@SuppressWarnings("serial")
+	public static class HelloServlet extends HttpServlet {
+		@Override
+		protected void doGet(HttpServletRequest request,
+				HttpServletResponse response) throws ServletException,
+				IOException {
+			response.setContentType("text/html");
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().println("<h1>Hello from HelloServlet</h1>");
+		}
+	}
 }
