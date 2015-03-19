3d04d417f70405f213cea3e603a9cc98876ccce9
nik <nikit.cpp@yandex.ru>
2015-03-19 19:46:52 +0300

    JUnit exception unable to find component

--- src/test/java/megatest/FirstGUITest.java
+++ src/test/java/megatest/FirstGUITest.java
@@ -6,6 +6,9 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import gui.MainWindow;
 
+import org.apache.log4j.Logger;
+import org.fest.swing.core.BasicRobot;
+import org.fest.swing.core.Robot;
 import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
 import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
@@ -15,31 +18,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.fest.swing.launcher.ApplicationLauncher.application;
 
+import static org.fest.swing.launcher.ApplicationLauncher.application;
+import static org.fest.swing.testing.FestSwingTestCaseTemplate.*;
 import vk.VkPlayListBuilderException;
 
 public class FirstGUITest {
 	 
 	  private JPanelFixture window;
+	  private Robot robot;
+	  
+	  private static Logger LOGGER = Logger.getLogger(FirstGUITest.class);
 	 
-	  /*@BeforeClass public static void setUpOnce() {
-	    FailOnThreadViolationRepaintManager.install();
+	  @BeforeClass public static void setUpOnce() {
+	    //FailOnThreadViolationRepaintManager.install();
 	  }
 	 
 	  @Before public void setUp() throws IOException {
-		  MainWindow frame = GuiActionRunner.execute(new GuiQuery<MainWindow>() {
-	        protected MainWindow executeInEDT() throws ParserConfigurationException, VkPlayListBuilderException {
-	          return new MainWindow();  
-	        }
-	    });
-	    window = new JPanelFixture(null, frame);
+		  application(MainWindow.class).start();
+		  robot = BasicRobot.robotWithCurrentAwtHierarchy();
+	    window = new JPanelFixture(robot, "List Model Example");
 	    //window.show(); // shows the frame to test
 	  }
 	 
 	  @After public void tearDown() {
-	    //window.cleanUp();
-	  }*/
+		  robot.cleanUp();
+	  }
 	 
 	  /*@Test public void shouldCopyTextInLabelWhenClickingButton() {
 	    window.textBox("textToCopy").enterText("Some random text");
@@ -48,7 +52,7 @@ public class FirstGUITest {
 	  }*/
 	  
 	  @Test public void test() throws IOException, InterruptedException {
-		  application(MainWindow.class).start();
+		  LOGGER.debug("Thread chech");;
 		    Thread.currentThread().join();
 		  }
 	}
