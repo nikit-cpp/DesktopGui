3534a8fb1cbc3f49948ef003f21dc3cff87fadae
nik <nikit.cpp@yandex.ru>
2015-03-23 04:59:58 +0300

    message "This message must be showed after song playing completed!"

--- src/main/java/service/PlayerService.java
+++ src/main/java/service/PlayerService.java
@@ -1,5 +1,7 @@
 package service;
 
+import org.apache.log4j.Logger;
+
 import player.Player;
 
 import com.google.common.eventbus.Subscribe;
@@ -7,12 +9,17 @@ import com.google.common.eventbus.Subscribe;
 import events.PlayEvent;
 
 public class PlayerService {
+	
+	private static Logger LOGGER = Logger.getLogger(PlayerService.class);
+
+	
 	private Player player;
 
 	@Subscribe
 	synchronized public void play(PlayEvent e){
 		player.prepareFor(e.getPath());
 		player.play();
+		LOGGER.debug("This message must be showed after song playing completed!");
 	}
 
 	public Player getPlayer() {
