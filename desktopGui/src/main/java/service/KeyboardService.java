6fb3bf938b6a9a968e87689db50714d980e434b5
nik <nikit.cpp@yandex.ru>
2015-04-15 23:41:05 +0300

    stupid bugs in sikulix2api - lib not loaded

--- desktopGui/src/main/java/service/KeyboardService.java
+++ desktopGui/src/main/java/service/KeyboardService.java
@@ -1,20 +1,24 @@
 package service;
 
 import java.awt.Event;
+import java.io.File;
+import java.io.IOException;
 
+import my.superpackage.LibLoader;
+import my.superpackage.LoadLibException;
+
+import org.sikuli.basics.FileManager;
 import org.sikuli.basics.HotkeyEvent;
 import org.sikuli.basics.HotkeyListener;
 import org.sikuli.basics.HotkeyManager;
+import org.sikuli.basics.LinuxHotkeyManager;
 import org.sikuli.script.RunTime;
 
-import com.melloware.jintellitype.JIntellitype;
-
 public class KeyboardService {
-	void m() {
-		//RunTime.loadLibrary("c:\\Programming\\Examples-java\\workspace\\SikuliX2_my\\lib\\windows\\JIntellitype.dll");
-		
-		JIntellitype.setLibraryLocation("c:\\Programming\\Examples-java\\workspace\\keyboard-windows\\lib\\JIntellitype.dll");
+	void m() throws IOException, InterruptedException, LoadLibException {
+		LibLoader.extractLib();
 		HotkeyManager hkm = HotkeyManager.getInstance();
+
 		hkm.addHotkey('v', Event.ALT_MASK, new HotkeyListener() {
 			
 			@Override
@@ -24,7 +28,7 @@ public class KeyboardService {
 		});
 	}
 
-	public static void main(String... args){
+	public static void main(String... args) throws IOException, InterruptedException, LoadLibException{
 		KeyboardService k = new KeyboardService();
 		k.m();
 	}
